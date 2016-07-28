ae64776c7131b6f95ba514ebcf853685b8e95d5a
sheep0128 <sheep0128@gmail.com>
2016-07-28 09:01:17 +0900

    Initial commit

---
+++ contact.py
@@ -0,0 +1,19 @@
+class Contact:
+    def __init__(self, name, phone_number, e_mail, addr):
+        self.name = name
+        self.phone_number = phone_number
+        self.e_mail = e_mail
+        self.addr = addr
+
+    def print_info(self):
+        print("Name: ", self.name)
+        print("Phone Number: ", self.phone_number)
+        print("E-mail: ", self.e_mail)
+        print("Address: ", self.addr)
+
+
+def main():
+    print("contact")
+
+if __name__ == "__main__":
+    main()
